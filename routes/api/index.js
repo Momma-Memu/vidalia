0fd4b27676a3d9a9fb311f09d115fc8c0e42652a
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-03 12:50:43 -0500

    quest route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const routes = ['session'];
+const routes = ['session', 'quests'];
 
 for (let route of routes) {
   router.use(`/${route}`, require(`./${route}`));
