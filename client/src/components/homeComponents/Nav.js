a744c68c4bb45c2f659bd161a88cab8d4998f6fa
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-03 12:59:09 -0500

    front end flow

--- client/src/components/homeComponents/Nav.js
+++ client/src/components/homeComponents/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tooltip from '@material-ui/core/Tooltip';
 import logo from '../../images/logo.png';
+import { NavLink } from 'react-router-dom'
 
 
 const Nav = () => {
@@ -13,7 +14,9 @@ const Nav = () => {
             <i className="fab fa-fort-awesome barracks-icon"></i>
             </Tooltip>
             <Tooltip title='Quests'>
-                <i className="fas fa-map-signs sign-icon"></i>
+                <NavLink exact to='/quests' className='nav-links'>
+                    <i className="fas fa-map-signs sign-icon"></i>
+                </NavLink>
             </Tooltip>
             <Tooltip title='More'>
                 <i className="fas fa-ellipsis-v more-icon"></i>
