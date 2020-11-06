31169dd549797883ab14a6dc423bc0808c8c98f7
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-06 13:37:39 -0500

    character maker route done

--- routes/api/create-character.js
+++ routes/api/create-character.js
@@ -15,4 +15,67 @@ router.get('/form-data-info', asyncHandler(async function (req, res, next) {
     return
 }));
 
+router.post('/make', asyncHandler(async function (req, res, next) {
+    const { name, story, abilityId, classId, creatorId  } = req.body;
+    let armorClass = 8;
+    let hitPoints = 8;
+    let strength = 8;
+    let dexterity = 8;
+    let constitution = 8;
+    let intelligence = 8;
+    let wisdom = 8;
+    let charisma = 8;
+
+    if(classId === 1){
+        armorClass += 3;
+        hitPoints += 3;
+        strength += 3;
+        constitution += 2;
+        intelligence -= 1;
+        wisdom -= 1;
+    } else if (classId === 2){
+        armorClass += 2;
+        hitPoints += 4;
+        strength += 2;
+        constitution += 3;
+        charisma += 1;
+    } else if (classId === 3) {
+        hitPoints += 1;
+        strength += 3;
+        dexterity += 3;
+        constitution += 1;
+        charisma += 3;
+    } else if (classId === 4){
+        hitPoints += 2;
+        strength += 1;
+        dexterity += 5;
+        constitution += 2;
+        intelligence += 1;
+        wisdom += 2;
+        charisma += 5;
+    } else if (classId === 5){
+        hitPoints += 1;
+        strength += 1;
+        dexterity += 5;
+        constitution += 2;
+        intelligence += 3;
+        wisdom +=3;
+        charisma +=2;
+    } else {
+        armorClass -= 1;
+        constitution += 4;
+        intelligence += 5;
+        wisdom += 5;
+        charisma += 3;
+    }
+
+    const newCharacter = { name, story, armorClass, hitPoints,
+    strength, dexterity, constitution, intelligence, wisdom, charisma,
+    abilityId, classId, creatorId }
+
+    const chara = await Character.create(newCharacter)
+
+    res.json(chara)
+}));
+
 module.exports = router;
