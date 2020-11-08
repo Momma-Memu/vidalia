09cbaf25dc06e9780735d945f22d3230ed2e929f
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-08 11:28:02 -0500

    beastiary

---
+++ client/src/components/homeComponents/Beastiary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Beastiary = () => {
+
+    return(
+        <div>hey girl hey</div>
+    )
+}
+
+export default Beastiary;
--- client/src/components/homeComponents/Home.js
+++ client/src/components/homeComponents/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from './Nav';
+import Beastiary from './Beastiary';
 
 const Home = () => {
 
     return (
         <div>
-        <Nav />
+            <Nav />
+            <Beastiary />
         </div>
     )
 }
