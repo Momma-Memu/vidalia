d9616f471a4abc92099c433f05ba0b330c0f5774
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-15 12:05:01 -0500

    hp bar color changes

--- client/src/helpers/hpBarChanger.js
+++ client/src/helpers/hpBarChanger.js
@@ -5,6 +5,14 @@ const hpBarChanger = (ref, ogHp, currentHp) => {
     const percent = (currentHp / ogHp) * 100;
     const styler = `${percent}%`;
     ref.current.style.width = styler;
+    if(percent <= 50 && percent > 25){
+        console.log('hello')
+        ref.current.style.backgroundColor = '#FFB02E';
+    } else if(percent <= 25){
+        ref.current.style.backgroundColor = '#F95454';
+    } else if(percent > 50) {
+        ref.current.style.backgroundColor = 'rgb(65, 163, 65)';
+    }
 }
 
 export default hpBarChanger;
