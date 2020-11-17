ea3ebe6d9f748d70f142179ee23a3375f3041b71
Miah Barnes <miahellenbarnes@gmail.com>
2020-11-17 10:11:10 -0500

    refactoring

--- client/src/components/survival/SurvivalSetup.js
+++ client/src/components/survival/SurvivalSetup.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { authContext } from '../../Context'
 import Footer from '../landingComponents/Footer';
+import SetupCard from './SetupCards';
 
 
 const SurvivalSetup = () => {
@@ -19,35 +20,7 @@ const SurvivalSetup = () => {
     }
 
 
-    const charCards = charList.map((char) => {
-        return (
-            <div className='char-card-container'>
-                <div className='survival-char-card-header'>
-                    <div>{char.name}</div>
-                    <div className='char-card-class-name'>{char.Class.name}</div>
-                </div>
-                <div className='survival-char-card-stats'>
-                    <div className='survival-char-card-stat1'>{`Armor Class: ${char.armorClass}`}</div>
-                    <div className='survival-char-card-stat2'>{`Charisma: ${char.charisma}`}</div>
-                    <div className='survival-char-card-stat1'>{`Constitution: ${char.constitution}`}</div>
-                    <div className='survival-char-card-stat2'>{`Dexterity: ${char.dexterity}`}</div>
-                    <div className='survival-char-card-stat1'>{`Hit Points: ${char.hitPoints}`}</div>
-                    <div className='survival-char-card-stat2'>{`Intelligence: ${char.intelligence}`}</div>
-                    <div className='survival-char-card-stat1'>{`Strength: ${char.strength}`}</div>
-                    <div className='survival-char-card-stat2'>{`Wisdom: ${char.wisdom}`}</div>
-                </div>
-                <div className='survival-char-card-footer'>
-                    <div>{`Ability: ${char.Ability.name}`}</div>
-                    <div>{`Weakness: ${char.Class.weakness}`}</div>
-                </div>
-                <NavLink to={`/survival/${char.id}`} className='button-links'>
-                    <div className='button-area-wrapper'>
-                        <div className='select-button'>Select</div>
-                    </div>
-                </NavLink>
-            </div>
-        )
-    })
+    const charCards = charList.map((char) => <SetupCard key={char.id} char={char} />)
 
     return (
         <div className='body'>
